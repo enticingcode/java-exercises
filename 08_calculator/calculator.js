7fd7bf5886cfe1dbb03445954f956853d996a447
Marvin <trujillomarvin@hotmail.com>
2021-09-28 19:49:28 -0700

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,21 +14,41 @@ const sum = function (numbers) {
   return x;
   }
 
-console.log(sum([]));
+console.log(sum([1, 2, 3]));
 
 
 
-const multiply = function () {
+const multiply = function (numbers) {
+  let x = numbers.reduce((total, currentValue) => {return total * currentValue})
+  return x;
+};
+console.log(multiply([3, 5]))
 
+const power = function (x, y) {
+  let result = Math.pow(x, y);
+  return result;
 };
 
-const power = function () {
+console.log(power(4, 3));
 
+const factorial = function (num) {
+  if (num < 0) {
+    return -1
+  } else if (num==0)  
+    return 1;
+    else {
+      return (num * factorial(num -1))
+    }
 };
 
-const factorial = function () {
+console.log(factorial(5));
 
-};
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
