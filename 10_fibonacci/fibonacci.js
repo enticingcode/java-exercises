b0c4c5dad2fb41c522bc27541827f3f82203842c
marvo <trujillomarvin@hotmail.com>
2021-10-01 14:39:51 -0700

    formula down

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    // F(n) = F(n-1) + F(n-2); Fib sequence formula
 };
+
+
+
+
+
+
+
+
+
+
+// fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6) // returns 
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+
